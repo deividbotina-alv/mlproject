4f47916877fd8fb7ef36e7f1ef3dbe8471ce38ad
diana <dianamarin228@gmail.com>
2023-07-21 21:48:59 +0200

    [ADD, UPD] data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,4 @@
-#Reading the data from data source#
+#Reading the data from data source, split and eventually save the data#
 import os
 import sys
 from src.exception import CustomException
@@ -8,9 +8,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
-    # Define where the data will be saved
+    """ It gives any path that I will require"""
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
@@ -48,4 +51,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
